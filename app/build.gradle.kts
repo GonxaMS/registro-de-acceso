ea09c32b57986688c3f6115c0e9b1ae6db2f2af8
GonxaMS <111082578+GonxaMS@users.noreply.github.com>
2026-09-15 10:05:13 -0300

    Bump Android release to 3.10.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.ejemplo.registroguardias"
         minSdk = 24
         targetSdk = 35
-        versionCode = 60
-        versionName = "3.10.6"
+        versionCode = 61
+        versionName = "3.10.7"
         buildConfigField("String", "SHEETS_WEB_URL", "\"${sheetsWebUrl.replace("\"", "\\\"")}\"")
         buildConfigField("boolean", "USE_FIREBASE_EMULATOR", useFirebaseEmulator.toString())
     }
